2b128d8a951c80e7c39a353901d83d1becd64c04
KwangSinG <kwangsing3@gmail.com>
2020-01-12 12:23:35 +0800

    Updated: update Node.js code and add description.

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const linebot = require('linebot');
 const express = require('express');
-const linebotParser = require('./bot.event')
+const linebotParser = require('./bot.event.js')
 
 // Connect to MongoDB
-mongoose
-  .connect(
-    'mongodb://使用者帳號:密碼@ds113495.mlab.com:13495/ninja-hattori',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+
 
 const app = express();
 // Handle static files
